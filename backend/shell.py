f89100466bfb1f6781c6a69e3ca09ef03d4fd379
Gaurav Singh <72973397+dev-north@users.noreply.github.com>
2022-04-21 13:40:30 +0530

    Delete backend directory

--- backend/shell.py
+++
@@ -1,8 +0,0 @@
-import hinglish
-
-while True:
-    text = input('hinglish > ')
-    result, error = hinglish.execute('<stdin>', text)
-
-    if error: print(error.to_string())
-    else: print(result)
